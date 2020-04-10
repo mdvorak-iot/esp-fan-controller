1cac1bc622c47f31166725f86f2286030a963387
Michal Dvorak <michal@mdvorak.org>
2020-04-10 11:20:18 +0200

    added Pwm class

---
+++ include/Pwm.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <stdint.h>
+#include <driver/gpio.h>
+#include <driver/ledc.h>
+
+class Pwm
+{
+  public:
+    Pwm(gpio_num_t pin, ledc_timer_t timer, ledc_channel_t channel, uint32_t frequency, ledc_timer_bit_t resolution)
+        : _timer(timer),
+          _channel(channel),
+          _pin(pin),
+          _frequency(frequency),
+          _resolution(resolution),
+          _maxDuty((1UL << _resolution) - 1),
+          _maxFrequency(80000000 / 2 ^ _resolution)
+    {
+        // Validate frequency
+        ESP_ERROR_CHECK(frequency <= _maxFrequency ? ESP_OK : ESP_FAIL);
+    }
+
+    esp_err_t begin()
+    {
+        // Power PWM
+        ledc_timer_config_t timerConfig = {};
+        timerConfig.timer_num = _timer;
+        timerConfig.speed_mode = LEDC_HIGH_SPEED_MODE;
+        timerConfig.duty_resolution = _resolution;
+        timerConfig.freq_hz = _frequency;
+        auto r = ledc_timer_config(&timerConfig);
+        if (r != ESP_OK)
+        {
+            return r;
+        }
+
+        // Power Channel
+        ledc_channel_config_t channelConfig = {};
+        channelConfig.timer_sel = _timer;
+        channelConfig.channel = _channel;
+        channelConfig.gpio_num = _pin;
+        channelConfig.speed_mode = LEDC_HIGH_SPEED_MODE;
+        channelConfig.duty = 1; // Note: sometimes during init driver is in inconsistent state - triggering it resets it properly to OFF state
+        r = ledc_channel_config(&channelConfig);
+        if (r != ESP_OK)
+        {
+            return r;
+        }
+        r = ledc_set_duty_and_update(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0, 0);
+        return r;
+    }
+    
+    uint32_t maxDuty() const
+    {
+        return _maxDuty;
+    }
+
+    uint32_t duty() const
+    {
+        return _duty;
+    }
+
+    esp_err_t duty(uint32_t duty)
+    {
+        // Avoid overflow
+        if (duty > _maxDuty )
+        {
+            duty = _maxDuty;
+        }
+        // NOOP
+        if (duty == _duty)
+        {
+            return ESP_OK;
+        }
+
+        // Update
+        auto r = ledc_set_duty_and_update(LEDC_HIGH_SPEED_MODE, _channel, duty, 0);
+        if (r == ESP_OK)
+        {
+            _duty = duty;
+        }
+        return r;
+    }
+
+    uint32_t frequency() const
+    {
+        return _frequency;
+    }
+
+#ifndef NDEBUG
+    esp_err_t frequency(uint32_t frequency)
+    {
+        ESP_ERROR_CHECK(frequency <= _maxFrequency ? ESP_OK : ESP_FAIL);
+
+        auto r = ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, _frequency);
+        if (r == ESP_OK)
+        {
+            _frequency = frequency;
+        }
+        return r;
+    }
+#endif
+
+  private:
+    ledc_timer_t _timer;
+    ledc_channel_t _channel;
+    gpio_num_t _pin;
+    uint32_t _frequency;
+    uint32_t _duty;
+    ledc_timer_bit_t _resolution;
+    uint32_t _maxDuty;
+    uint32_t _maxFrequency;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include "Pwm.h"
 
-void setup() {
-  // put your setup code here, to run once:
+const auto PWM_PIN = GPIO_NUM_25;
+const uint32_t PWM_FREQ = 300000;
+const auto PWM_RESOLUTION = LEDC_TIMER_9_BIT;
+
+static Pwm pwm(PWM_PIN, LEDC_TIMER_0, LEDC_CHANNEL_0, PWM_FREQ, PWM_RESOLUTION);
+
+void setup()
+{
+  // Enable ISR
+  ESP_ERROR_CHECK(gpio_install_isr_service(0));
+
+  // Initialize LEDC
+  ESP_ERROR_CHECK(ledc_fade_func_install(0));
+
+  // Power PWM
+  ESP_ERROR_CHECK(pwm.begin());
+  ESP_ERROR_CHECK(pwm.duty(pwm.maxDuty()));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Read temperatures
+
+  // Read external PWM signal
+  // TODO pulseIn()
+
+  // Control PWM
+  uint32_t dutyPercent = 100;
+  pwm.duty(dutyPercent * pwm.maxDuty() / 100U);
 }
