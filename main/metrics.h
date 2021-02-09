30c99887b3ed1083d5fd98bda71baf44c711ff86
Michal Dvorak <michal@mdvorak.org>
2021-02-09 19:21:44 +0100

    metric_value.label overrides

--- main/metrics.h
+++ main/metrics.h
@@ -33,6 +33,48 @@ public:
         return *this;
     }
 
+    inline metric_value &label(const char *name, const std::string &value)
+    {
+        begin_label(name) << value << '"';
+        return *this;
+    }
+
+    inline metric_value &label(const char *name, int value)
+    {
+        begin_label(name) << value << '"';
+        return *this;
+    }
+
+    inline metric_value &label(const char *name, int64_t value)
+    {
+        begin_label(name) << value << '"';
+        return *this;
+    }
+
+    inline metric_value &label(const char *name, uint64_t value)
+    {
+        begin_label(name) << value << '"';
+        return *this;
+    }
+
+    inline metric_value &label_hex(const char *name, uint8_t value)
+    {
+        begin_label(name) << std::setfill('0') << std::setw(2) << std::uppercase << std::hex << std::right << value << '"';
+        return *this;
+    }
+
+    inline metric_value &label_hex(const char *name, uint16_t value)
+    {
+        begin_label(name) << std::setfill('0') << std::setw(4) << std::uppercase << std::hex << std::right << value << '"';
+        return *this;
+    }
+
+    inline metric_value &label_hex(const char *name, uint32_t value)
+    {
+        begin_label(name) << std::setfill('0') << std::setw(8) << std::uppercase << std::hex << std::right << value << '"';
+        return *this;
+    }
+
     inline metric_value &label_hex(const char *name, uint64_t value)
     {
         begin_label(name) << std::setfill('0') << std::setw(16) << std::uppercase << std::hex << std::right << value << '"';
