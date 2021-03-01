3eeeab4817acf650df356053677cfcfeebdad50b
Michal Dvorak <michal@mdvorak.org>
2021-03-01 18:14:00 +0100

    smart context handling

--- components/serialization/include/serialization_context_cjson.hpp
+++ components/serialization/include/serialization_context_cjson.hpp
@@ -14,34 +14,56 @@ struct serialization_context_cjson : public serialization_context
         assert(obj);
     }
 
-    esp_err_t get_i8(const char *key, int8_t *out_value) const override;
-    esp_err_t get_u8(const char *key, uint8_t *value) const override;
-    esp_err_t get_i16(const char *key, int16_t *out_value) const override;
-    esp_err_t get_u16(const char *key, uint16_t *out_value) const override;
-    esp_err_t get_i32(const char *key, int32_t *out_value) const override;
-    esp_err_t get_u32(const char *key, uint32_t *out_value) const override;
-    esp_err_t get_i64(const char *key, int64_t *out_value) const override;
-    esp_err_t get_u64(const char *key, uint64_t *out_value) const override;
-    esp_err_t get_bool(const char *key, bool *value) const override;
-    esp_err_t get_float(const char *key, float *out_value) const override;
-    esp_err_t get_double(const char *key, double *out_value) const override;
-    esp_err_t get_str(const char *key, char *out_value, size_t *out_value_len) const override;
-    esp_err_t get_blob(const char *key, uint8_t *out_value, size_t *out_value_len) const override;
+    serialization_context &name(const char *key) override
+    {
+        key_ = key;
+        return *this;
+    }
 
-    esp_err_t set_i8(const char *key, int8_t value) const override;
-    esp_err_t set_u8(const char *key, uint8_t value) const override;
-    esp_err_t set_i16(const char *key, int16_t value) const override;
-    esp_err_t set_u16(const char *key, uint16_t value) const override;
-    esp_err_t set_i32(const char *key, int32_t value) const override;
-    esp_err_t set_u32(const char *key, uint32_t value) const override;
-    esp_err_t set_i64(const char *key, int64_t value) const override;
-    esp_err_t set_u64(const char *key, uint64_t value) const override;
-    esp_err_t set_bool(const char *key, bool value) const override;
-    esp_err_t set_float(const char *key, float value) const override;
-    esp_err_t set_double(const char *key, double value) const override;
-    esp_err_t set_str(const char *key, const char *value) const override;
-    esp_err_t set_blob(const char *key, const uint8_t *value, size_t value_len) const override;
+    serialization_context &nested_context(const char *name) override
+    {
+        return *this;
+    }
+
+    serialization_context &add() override
+    {
+        return *this;
+    }
+
+    serialization_context &leave_context() override
+    {
+        return *this;
+    }
+
+    esp_err_t get_i8(int8_t *out_value) const override;
+    esp_err_t get_u8(uint8_t *value) const override;
+    esp_err_t get_i16(int16_t *out_value) const override;
+    esp_err_t get_u16(uint16_t *out_value) const override;
+    esp_err_t get_i32(int32_t *out_value) const override;
+    esp_err_t get_u32(uint32_t *out_value) const override;
+    esp_err_t get_i64(int64_t *out_value) const override;
+    esp_err_t get_u64(uint64_t *out_value) const override;
+    esp_err_t get_bool(bool *value) const override;
+    esp_err_t get_float(float *out_value) const override;
+    esp_err_t get_double(double *out_value) const override;
+    esp_err_t get_str(char *out_value, size_t *out_value_len) const override;
+    esp_err_t get_blob(uint8_t *out_value, size_t *out_value_len) const override;
+
+    esp_err_t set_i8(int8_t value) const override;
+    esp_err_t set_u8(uint8_t value) const override;
+    esp_err_t set_i16(int16_t value) const override;
+    esp_err_t set_u16(uint16_t value) const override;
+    esp_err_t set_i32(int32_t value) const override;
+    esp_err_t set_u32(uint32_t value) const override;
+    esp_err_t set_i64(int64_t value) const override;
+    esp_err_t set_u64(uint64_t value) const override;
+    esp_err_t set_bool(bool value) const override;
+    esp_err_t set_float(float value) const override;
+    esp_err_t set_double(double value) const override;
+    esp_err_t set_str(const char *value) const override;
+    esp_err_t set_blob(const uint8_t *value, size_t value_len) const override;
 
  protected:
     cJSON *const obj;
+    const char *key_;
 };
--- components/serialization/include/serialization_context_nvs.hpp
+++ components/serialization/include/serialization_context_nvs.hpp
@@ -8,40 +8,73 @@ struct serialization_context_nvs : public serialization_context
 {
  public:
     explicit serialization_context_nvs(nvs_handle_t handle, const char *prefix = "")
-        : handle(handle), prefix(prefix)
+        : handle_(handle), prefix_(prefix), index_(SIZE_MAX)
     {
         assert(handle);
     }
 
-    esp_err_t get_i8(const char *key, int8_t *out_value) const override;
-    esp_err_t get_u8(const char *key, uint8_t *value) const override;
-    esp_err_t get_i16(const char *key, int16_t *out_value) const override;
-    esp_err_t get_u16(const char *key, uint16_t *out_value) const override;
-    esp_err_t get_i32(const char *key, int32_t *out_value) const override;
-    esp_err_t get_u32(const char *key, uint32_t *out_value) const override;
-    esp_err_t get_i64(const char *key, int64_t *out_value) const override;
-    esp_err_t get_u64(const char *key, uint64_t *out_value) const override;
-    esp_err_t get_bool(const char *key, bool *value) const override;
-    esp_err_t get_float(const char *key, float *out_value) const override;
-    esp_err_t get_double(const char *key, double *out_value) const override;
-    esp_err_t get_str(const char *key, char *out_value, size_t *out_value_len) const override;
-    esp_err_t get_blob(const char *key, uint8_t *out_value, size_t *out_value_len) const override;
+    serialization_context &name(const char *key) override
+    {
+        key_ = prefix_ + key;
+        return *this;
+    }
 
-    esp_err_t set_i8(const char *key, int8_t value) const override;
-    esp_err_t set_u8(const char *key, uint8_t value) const override;
-    esp_err_t set_i16(const char *key, int16_t value) const override;
-    esp_err_t set_u16(const char *key, uint16_t value) const override;
-    esp_err_t set_i32(const char *key, int32_t value) const override;
-    esp_err_t set_u32(const char *key, uint32_t value) const override;
-    esp_err_t set_i64(const char *key, int64_t value) const override;
-    esp_err_t set_u64(const char *key, uint64_t value) const override;
-    esp_err_t set_bool(const char *key, bool value) const override;
-    esp_err_t set_float(const char *key, float value) const override;
-    esp_err_t set_double(const char *key, double value) const override;
-    esp_err_t set_str(const char *key, const char *value) const override;
-    esp_err_t set_blob(const char *key, const uint8_t *value, size_t value_len) const override;
+    serialization_context &nested(const char *name) override
+    {
+        prefix_ = name;
+        return *this;
+    }
+
+    serialization_context &add() override
+    {
+        if (index_ == SIZE_MAX)
+        {
+            index_ = 0;
+        }
+        else
+        {
+            index_++;
+        }
+        return *this;
+    }
+
+    serialization_context &leave_context() override
+    {
+        prefix_ = std::string();
+        return *this;
+    }
+
+    esp_err_t get_i8(int8_t *out_value) const override;
+    esp_err_t get_u8(uint8_t *value) const override;
+    esp_err_t get_i16(int16_t *out_value) const override;
+    esp_err_t get_u16(uint16_t *out_value) const override;
+    esp_err_t get_i32(int32_t *out_value) const override;
+    esp_err_t get_u32(uint32_t *out_value) const override;
+    esp_err_t get_i64(int64_t *out_value) const override;
+    esp_err_t get_u64(uint64_t *out_value) const override;
+    esp_err_t get_bool(bool *value) const override;
+    esp_err_t get_float(float *out_value) const override;
+    esp_err_t get_double(double *out_value) const override;
+    esp_err_t get_str(char *out_value, size_t *out_value_len) const override;
+    esp_err_t get_blob(uint8_t *out_value, size_t *out_value_len) const override;
+
+    esp_err_t set_i8(int8_t value) const override;
+    esp_err_t set_u8(uint8_t value) const override;
+    esp_err_t set_i16(int16_t value) const override;
+    esp_err_t set_u16(uint16_t value) const override;
+    esp_err_t set_i32(int32_t value) const override;
+    esp_err_t set_u32(uint32_t value) const override;
+    esp_err_t set_i64(int64_t value) const override;
+    esp_err_t set_u64(uint64_t value) const override;
+    esp_err_t set_bool(bool value) const override;
+    esp_err_t set_float(float value) const override;
+    esp_err_t set_double(double value) const override;
+    esp_err_t set_str(const char *value) const override;
+    esp_err_t set_blob(const uint8_t *value, size_t value_len) const override;
 
  protected:
-    nvs_handle_t const handle;
-    const std::string prefix;
+    nvs_handle_t const handle_;
+    std::string prefix_;
+    std::string key_;
+    size_t index_;
 };
