06aab6dd388444e79bac92043b0d45d66ad6b5a2
Michal Dvorak <michal@mdvorak.org>
2021-03-06 18:12:53 +0100

    app configs keys use json lowerCase notation

--- main/app_config.h
+++ main/app_config.h
@@ -58,21 +58,21 @@ esp_err_t app_config_add_to(const app_config_t *cfg, cJSON *data);
 char *app_config_print_address(char *buf, size_t buf_len, uint64_t value);
 
 #define APP_CONFIG_NVS_NAME "app_config"
-#define APP_CONFIG_KEY_STATUS_LED_PIN "status_led_pin"
-#define APP_CONFIG_KEY_STATUS_LED_ON_STATE "status_led_on"
-#define APP_CONFIG_KEY_PWM_PIN "pwm_pin"
-#define APP_CONFIG_KEY_PWM_INVERTED_DUTY "pwm_inverted"
-#define APP_CONFIG_KEY_RPM_PINS "rpm_pins"
-#define APP_CONFIG_KEY_SENSORS_PIN "sensors_pin"
-#define APP_CONFIG_KEY_PRIMARY_SENSOR_ADDRESS "primary_addr"
+#define APP_CONFIG_KEY_STATUS_LED_PIN "statusLedPin"
+#define APP_CONFIG_KEY_STATUS_LED_ON_STATE "statusLedOnState"
+#define APP_CONFIG_KEY_PWM_PIN "pwmPin"
+#define APP_CONFIG_KEY_PWM_INVERTED_DUTY "pwmInverted"
+#define APP_CONFIG_KEY_RPM_PINS "rpmPins"
+#define APP_CONFIG_KEY_SENSORS_PIN "sensorsPin"
+#define APP_CONFIG_KEY_PRIMARY_SENSOR_ADDRESS "primaryAddr"
 #define APP_CONFIG_KEY_SENSORS "sensors"
 #define APP_CONFIG_KEY_SENSOR_ADDRESS "addr"
 #define APP_CONFIG_KEY_SENSOR_NAME "name"
-#define APP_CONFIG_KEY_SENSOR_OFFSET_C "offset_c"
-#define APP_CONFIG_KEY_LOW_THRESHOLD_CELSIUS "lo_thresh_c"
-#define APP_CONFIG_KEY_HIGH_THRESHOLD_CELSIUS "hi_thresh_c"
-#define APP_CONFIG_KEY_LOW_THRESHOLD_DUTY_PERCENT "lo_thresh_duty"
-#define APP_CONFIG_KEY_HIGH_THRESHOLD_DUTY_PERCENT "hi_thresh_duty"
+#define APP_CONFIG_KEY_SENSOR_OFFSET_C "offsetC"
+#define APP_CONFIG_KEY_LOW_THRESHOLD_CELSIUS "lowThreshC"
+#define APP_CONFIG_KEY_HIGH_THRESHOLD_CELSIUS "highThreshC"
+#define APP_CONFIG_KEY_LOW_THRESHOLD_DUTY_PERCENT "lowThreshDuty"
+#define APP_CONFIG_KEY_HIGH_THRESHOLD_DUTY_PERCENT "highThreshDuty"
 
 #ifdef __cplusplus
 }
