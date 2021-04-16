21701545d7853cf5321238d6a94fb1d90ba7e52c
Michal Dvorak <mikee2185@gmail.com>
2021-04-16 21:46:16 +0200

    docs for fan_control_set_duty

--- main/fan_control.h
+++ main/fan_control.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <esp_err.h>
 #include <hal/gpio_types.h>
 #include <hal/ledc_types.h>
@@ -9,6 +10,13 @@ extern "C" {
 
 esp_err_t fan_control_config(gpio_num_t pin, ledc_timer_t timer, ledc_channel_t channel);
 
+/**
+ * Sets the duty according to percentual value.
+ *
+ * @param channel Channel which was used to configure the PWM.
+ * @param duty_percent Duty value from 0.0 to 1.0
+ * @return ESP_OK on success.
+ */
 esp_err_t fan_control_set_duty(ledc_channel_t channel, float duty_percent);
 
 #ifdef __cplusplus
